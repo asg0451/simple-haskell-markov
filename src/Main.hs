9ebd61aa7d61bec16a4d0814a955f1095a5fa78b
Miles Frankel <miles.frankel@gmail.com>
2016-08-01 16:21:26 -0400

    file argument

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import System.Random (randomRIO)
 import System.IO
 import Data.Maybe (fromJust, isNothing)
 import qualified Data.Set as S
-
+import System.Environment (getArgs)
 import System.Directory (doesFileExist)
 import qualified Data.ByteString as BS
 import Data.Serialize
@@ -23,8 +23,8 @@ getMapping mMap input = do
     Just res -> sample res
 
 sample :: S.Set a -> IO (Maybe a)
-sample list = if S.null list then return Nothing
-              else randomRIO (0, length list - 1) >>= return . Just . (`S.elemAt` list)
+sample set = if S.null set then return Nothing
+             else randomRIO (0, length set - 1) >>= return . Just . (`S.elemAt` set)
 
 genMap :: String -> MarkovMap
 genMap lexicon =
@@ -64,26 +64,29 @@ genString mMap input = go $ reverse $ take chainLength input
              | otherwise -> go $ fromJust maybeMapping : acc
 
 main :: IO ()
-main =  withFile "lexicon.txt" ReadMode $ \h -> do
-  lexiContents <- hGetContents h
-  mapFileExists <- doesFileExist "markov.cache"
-  markovMap <- if mapFileExists
-               then do putStrLn "Reading cache.."
-                       c <- BS.hGetContents =<< openFile "markov.cache" ReadMode
-                       case decode c of
-                         Left e -> do putStrLn "couldn't read cache, rebuilding"
-                                      buildMap lexiContents
-                         Right m -> return m
-               else buildMap lexiContents
-  putStrLn $ "loading done. please type " ++ show chainLength ++ " words"
-  forever $ do
-    c <- getLine
-    let ws = words c
-    print =<< genString markovMap (take chainLength ws)
+main =  do
+  args <- getArgs
+  let lexiconName = if not $ null args then head args else "lexicon.txt"
+  withFile lexiconName ReadMode $ \h -> do
+    lexiContents <- hGetContents h
+    mapFileExists <- doesFileExist $ lexiconName ++ ".cache"
+    markovMap <- if mapFileExists
+                 then do putStrLn "Reading cache.."
+                         c <- BS.hGetContents =<< openFile (lexiconName ++ ".cache") ReadMode
+                         case decode c of
+                           Left _ -> do putStrLn "couldn't read cache, rebuilding"
+                                        buildMap lexiconName lexiContents
+                           Right m -> return m
+                 else buildMap lexiconName lexiContents
+    putStrLn $ "loading done. please type " ++ show chainLength ++ " words"
+    forever $ do
+      c <- getLine
+      let ws = words c
+      print =<< genString markovMap (take chainLength ws)
 
-      where buildMap c = do putStrLn "Building map and writing cache.."
-                            let mMap = genMap c
-                            openFile "markov.cache" WriteMode >>= \h -> BS.hPut h (encode mMap)
-                            return mMap
-
+        where buildMap lexiName c = do putStrLn "Building map and writing cache.."
+                                       let mMap = genMap c
+                                       openFile (lexiName ++ ".cache") WriteMode >>= \h -> BS.hPut h (encode mMap)
+                                       return mMap
+chainLength :: Int
 chainLength = 3
